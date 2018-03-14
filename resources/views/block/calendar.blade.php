2a79fb5f68dcdb78697f2e1dc6eb781067a039ce
user2 <admin@massagery24.ru>
2018-03-14 17:41:15 +0300

    Новый город1.01

--- resources/views/block/calendar.blade.php
+++ resources/views/block/calendar.blade.php
@@ -1,15 +1,15 @@
 <link href="{{ URL::asset('public/block/calendar/style.css') }}" rel="stylesheet" type="text/css" >
 {{--<link rel="stylesheet" type="text/css" href="{{ asset('/block/calendar/style.css') }}" />--}}
 <i class="fa fa-calendar-o" aria-hidden="true">1</i>
-<i class="fa fa-angle-double-right" aria-hidden="true">1</i>
+
 <div class="calendar" data-count="">
     <div class="calendar_left_button left">
-
+        <i class="fa fa-angle-double-right fa-3x" aria-hidden="true"></i>
     </div>
     <div class="calendar_center">
         <div class="calendar_title">
-            <div class="title_day">д</div>
-            <div class="title_month">м</div>
+            <div class="title_day"><i class="fa fa-calendar-o" aria-hidden="true"></i></div>
+            <div class="title_month"><i class="fa fa-calendar" aria-hidden="true"></i></div>
         </div>
         <div class="calendar_value">
             <div class="value_day">1</div>
